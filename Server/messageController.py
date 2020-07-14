9d7d79c2c07ad8dd0512607f783f9c1f53e165ed
Maciek <Maciek@DESKTOP-7USF534>
2020-07-14 18:40:11 +0200

    Installed socketio dependencies

--- Server/messageController.py
+++ Server/messageController.py
@@ -2,11 +2,15 @@ import flask
 from models.message import Message
 from flask import request, jsonify
 from flask_cors import CORS, cross_origin
+from flask_socketio import SocketIO
+from flask_socketio import send, emit
 
 app = flask.Flask(__name__)
 # CORS(app, resources ={r"/*":{"origins":"*"}})
 CORS(app)
 app.config["DEBUG"] = True
+app.config['SECRET_KEY']='such_a_secret'
+socketio = SocketIO(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
@@ -39,10 +43,18 @@ def getMessages():
 @app.route('/post', methods=['POST'])
 def post():
     response = request.json
-    message = Message(**response)
-    messages.append(message)
-    print(message.message, message.user)
-    print(len(messages))
     return response
 
-app.run()
+@socketio.on('json', namespace='/test')
+def send_message(json):
+    messageObj = Message(**json)
+    messages.append(messageObj)
+    print(messageObj.message, messageObj.user)
+    print(len(messages))
+    send(messageObj, json = True, namespace = '/test', skip_sid=True)
+
+# @socketio.on('message')
+# def handle_message(message):
+#     print('received message: ' + message)
+
+socketio.run(app)
